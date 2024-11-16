be7ed30cd4fcd1e4b6db5c1fcb58b71573d32643
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2024-11-16 17:45:38 +0530

    urls.py

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from blog_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blog_app.urls')),
+    path('', views.home, name='home'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
