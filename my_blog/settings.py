cbb5d8d7721381b1c4db42fb2900f29296f6891a
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2024-11-16 16:26:19 +0530

    settings.py

--- my_blog/settings.py
+++ my_blog/settings.py
@@ -1,1 +1,10 @@
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'my_blog_db',
+        'USER': 'root',
+        'PASSWORD': 'root@1234',
+        'HOST': '172.31.24.50',
+        'PORT': '3306',
+    }
+}
