687608dff4c8f3deead45221106cbc0dd9f32123
Ali Naveed <114981749+alinaveed1983@users.noreply.github.com>
2024-11-16 17:41:56 +0530

    views.py

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,1 +1,10 @@
+from django.shortcuts import render
+from .models import BlogPost
 
+def home(request):
+    posts = BlogPost.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = BlogPost.objects.get(pk=pk)
+    return render(request, 'post_detail.html', {'post': post})
